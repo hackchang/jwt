0412427bd0a66dbe3e21a190a583e3bef1827af0
hackchang <haechang__@naver.com>
2020-09-20 17:57:05 +0900

    JWT Settings

---
+++ src/test/java/com/haechang/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haechang.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
